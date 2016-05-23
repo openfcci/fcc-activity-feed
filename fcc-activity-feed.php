132f50b52f179499d4bd63d1a18a8f91f6d61e66
ryan.veitch <ryan.r.veitch@gmail.com>
2016-05-22 23:49:13 -0500

    Code Cleanup - WP Standards
    - Cleaned up syntax to WP PHP Standards

--- fcc-activity-feed.php
+++ fcc-activity-feed.php
@@ -4,7 +4,7 @@ Plugin Name: FCC Activity Feed
 Plugin URI: https://github.com/openfcci/fcc-activity-feed
 Description: An activity feed for displaying global network posts.
 Author: Forum Communications Company
-Version: 1.16.04.08
+Version: 1.16.05.22
 Author URI: http://forumcomm.com/
 */
 
@@ -12,16 +12,16 @@ define( 'ACTFEED__PLUGIN_DIR',  plugin_dir_url( __FILE__ ) );
 
 function activityfeed_add_lightbox() {
 	if ( ! is_admin() && ! is_home() ) {
-		$pagename = get_query_var('pagename');
-		if ( $pagename == 'activity' ){
-			wp_enqueue_style(  'wp-featherlight', ACTFEED__PLUGIN_DIR . 'includes/wp-featherlight.min.css' );
+		$pagename = get_query_var( 'pagename' );
+		if ( 'activity' == $pagename ) {
+			wp_enqueue_style( 'wp-featherlight', ACTFEED__PLUGIN_DIR . 'includes/wp-featherlight.min.css' );
 			wp_enqueue_script( 'wp-featherlight', ACTFEED__PLUGIN_DIR . 'includes/wpFeatherlight.pkgd.min.js' );
 		}
 	} else {
 		// Don't add lightbox
 	}
 }
-add_action('wp_head', 'activityfeed_add_lightbox');
+add_action( 'wp_head', 'activityfeed_add_lightbox' );
 
 /*
 Usage:
@@ -30,7 +30,7 @@ solr_display_recent_posts(NUMBER,TITLE_CHARACTERS,CONTENT_CHARACTERS,TITLE_CONTE
 Ex:
 solr_display_recent_posts(10,40,150,'<br />','<strong>','</strong>','<ul>','</ul>','<li>','</li>','yes','yes',16, 'post', true);
 */
-class solractivityfeedshortcode {
+class SolrActivityFeedShortcode {
 
 	var $build = 1;
 
@@ -42,12 +42,12 @@ class solractivityfeedshortcode {
 
 		$this->db =& $wpdb;
 
-		if($this->db->blogid == 1) {
+		if ( 1 == $this->db->blogid ) {
 			// Only add the feed for the main site
-			add_action('init', array(&$this, 'initialise_solractivityfeedshortcode') );
+			add_action( 'init', array( &$this, 'initialise_solractivityfeedshortcode' ) );
 		}
 
-		add_shortcode( 'solrrecentposts', array( &$this, 'solr_display_recent_posts_shortcode') );
+		add_shortcode( 'solrrecentposts', array( &$this, 'solr_display_recent_posts_shortcode' ) );
 
 	}
 
@@ -59,37 +59,37 @@ class solractivityfeedshortcode {
 		// In case we need it in future :)
 	}
 
-	function solr_display_recent_posts($tmp_number,$tmp_title_characters = 0,$tmp_content_characters = 0,$tmp_title_content_divider = '<br />',$tmp_title_before,$tmp_title_after,$tmp_global_before,$tmp_global_after,$tmp_before,$tmp_after,$tmp_title_link = 'no',$tmp_show_avatars = 'yes', $tmp_avatar_size = 16, $posttype = 'post', $output = true) {
+	function solr_display_recent_posts( $tmp_number, $tmp_title_characters = 0, $tmp_content_characters = 0, $tmp_title_content_divider = '<br />', $tmp_title_before, $tmp_title_after, $tmp_global_before, $tmp_global_after, $tmp_before, $tmp_after, $tmp_title_link = 'no', $tmp_show_avatars = 'yes', $tmp_avatar_size = 16, $posttype = 'post', $output = true ) {
 
 		$html = '';
 
 		/***  JSON Loop Starts Here ***/
-		$default_avatar = get_option('default_avatar');
+		$default_avatar = get_option( 'default_avatar' );
 
 		#JSON
-		$response = file_get_contents('http://avsearch.fccinteractive.com:8080/solr/select?indent=on&version=2.2&q=*%3A*&fq=-blogid%3A1&fq=publishtime%3A%5BNOW-7DAY+TO+NOW%5D&fq=type%3Apost&start=0&rows=' . $tmp_number .'&fl=id%2C+permalink%2C+blogid%2C+title%2C+author%2C+type%2C+publishtime%2C+categories&qt=&wt=json&sort=publishtime+desc&omitHeader=true');
-		$response = json_decode($response, true);
+		$response = file_get_contents( 'http://avsearch.fccinteractive.com:8080/solr/select?indent=on&version=2.2&q=*%3A*&fq=-blogid%3A1&fq=publishtime%3A%5BNOW-7DAY+TO+NOW%5D&fq=type%3Apost&start=0&rows=' . $tmp_number .'&fl=id%2C+permalink%2C+blogid%2C+title%2C+author%2C+type%2C+publishtime%2C+categories&qt=&wt=json&sort=publishtime+desc&omitHeader=true' );
+		$response = json_decode( $response, true );
 		$docs = $response['response']['docs'];
-		$returnedposts = count($docs);
+		$returnedposts = count( $docs );
 		$totalposts = $response['response']['numFound'];
 
-		$html .= '<h3 class="foot-head">Latest Posts</h3><div class="relative" style="text-align: left;">' . 'Showing ' . $returnedposts . ' of ' . $totalposts .' posts published since ' . date('m/d/Y', strtotime('-7 days')) . '.</div>';
+		$html .= '<h3 class="foot-head">Latest Posts</h3><div class="relative" style="text-align: left;">' . 'Showing ' . $returnedposts . ' of ' . $totalposts .' posts published since ' . date( 'm/d/Y', strtotime( '-7 days' ) ) . '.</div>';
 		$html .= $tmp_global_before;
 
-		for($i = 0; $i < count($docs); $i++){
+		for ( $i = 0; $i < count( $docs ); $i++ ) {
 
-			$obj = $docs[$i];
+			$obj = $docs[ $i ];
 
 			$html .= $tmp_before;
 			if ( $tmp_title_characters > 0 ) {
 				$html .= $tmp_title_before;
 
 				# Article Title
-				$feed_title =$obj['title'];
+				$feed_title = $obj['title'];
 				# Article ID
-				$feed_id = preg_replace('/[^0-9]/','',$obj['id']);
+				$feed_id = preg_replace( '/[^0-9]/','',$obj['id'] );
 				# Article Blog ID
-				if ( $obj['blogid'] === '0') {
+				if ( '0' === $obj['blogid'] ) {
 					# Set SayAnythingBlog to correct blog_id
 					$feed_blogid = '67725';
 				} else {
@@ -106,20 +106,20 @@ class solractivityfeedshortcode {
 				# Article Category
 				$feed_category = $obj['categories'][0];
 				# Article Time
-				$post_time = gmdate( 'm/d/Y g:i a', strtotime($feed_publishtime));
+				$post_time = gmdate( 'm/d/Y g:i a', strtotime( $feed_publishtime ) );
 
 				# Feed Site
 				//$feed_site = get_blog_details( $feed_id )->path;
-				$feed_site = preg_replace('/www\./','',preg_replace('/\/()\d+/','',$obj['id']));
+				$feed_site = preg_replace( '/www\./','',preg_replace( '/\/()\d+/','',$obj['id'] ) );
 
 				# Placeholder Image
 				$placeholder_image_url = ACTFEED__PLUGIN_DIR . 'placeholder.jpeg';
 				$placeholder_image = '<a href="' .$placeholder_image_url . '?TB_iframe=true" class="thickbox">' . '<img src="' . $placeholder_image_url . '">' . '</a>';
 
 				# Featured Image
-				$featured_image = NULL;
-				if ( $obj['blogid'] === '0') {
-					switch ($feed_site) {
+				$featured_image = null;
+				if ( '0' === $obj['blogid'] ) {
+					switch ( $feed_site ) {
 					    case 'sayanythingblog.com':
 					        $featured_image = 'https://i0.wp.com/www.sayanythingblog.com/files/2016/04/sab-activityfeed.png?zoom=2&fit=95%2C35';
 					        break;
@@ -127,7 +127,7 @@ class solractivityfeedshortcode {
 					        $featured_image = 'https://i2.wp.com/www.northlandoutdoors.com/files/2015/12/cropped-Northland-Outdoors-Site-Icon.png?resize=95%2C35';
 					        break;
 					    case 'bakkentoday.com':
-					        $featured_image =  'https://i0.wp.com/bakkentoday.areavoices.com/wp-content/blogs.dir/67724/files/2016/03/cropped-Bakken-Today-Site-Icon.png?resize=95%2C35';
+					        $featured_image = 'https://i0.wp.com/bakkentoday.areavoices.com/wp-content/blogs.dir/67724/files/2016/03/cropped-Bakken-Today-Site-Icon.png?resize=95%2C35';
 					        break;
 					    default:
 					        $featured_image = $placeholder_image_url;
@@ -138,7 +138,7 @@ class solractivityfeedshortcode {
 				} else {
 					switch_to_blog( $feed_blogid );
 					$featured_image = get_the_post_thumbnail( $feed_id, 'small-thumb' );
-					$post_thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id($feed_id) );
+					$post_thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $feed_id ) );
 					$featured_image_with_url = '<a href="' . $post_thumbnail_url . '?TB_iframe=true" class="thickbox">' . $featured_image . '</a>';
 					restore_current_blog();
 				}
@@ -151,16 +151,15 @@ class solractivityfeedshortcode {
 				}
 
 				# Title
-				if ( $tmp_title_link == 'no' ) {
-					$html .= substr($feed_title,0,$tmp_title_characters);
-				}
-				else {
+				if ( 'no' == $tmp_title_link  ) {
+					$html .= substr( $feed_title,0,$tmp_title_characters );
+				} else {
 					if ( ! $featured_image ) {
 						$html .= '<div class="sidebar-list-text left relative">';
-						$html .= '<a href="' . $feed_permalink . '" target="_blank" style="color: #888;">' . substr($feed_title,0,$tmp_title_characters) . '</a>';
+						$html .= '<a href="' . $feed_permalink . '" target="_blank" style="color: #888;">' . substr( $feed_title,0,$tmp_title_characters ) . '</a>';
 					} else {
 						$html .= '<div class="sidebar-list-text left relative">';
-						$html .= '<a href="' . $feed_permalink . '" target="_blank">' . substr($feed_title,0,$tmp_title_characters) . '</a>';
+						$html .= '<a href="' . $feed_permalink . '" target="_blank">' . substr( $feed_title,0,$tmp_title_characters ) . '</a>';
 					}
 				}
 
@@ -188,8 +187,7 @@ class solractivityfeedshortcode {
 		}
 		$html .= $tmp_global_after;
 
-
-		if($output) {
+		if ( $output ) {
 			echo $html;
 		} else {
 			return $html;
@@ -197,40 +195,40 @@ class solractivityfeedshortcode {
 
 	}
 
-	function solr_display_recent_posts_shortcode($atts, $content = null, $code = "") {
+	function solr_display_recent_posts_shortcode( $atts, $content = null, $code = '' ) {
 
-		$defaults = array(	'number'	=>	5,
-							'title_characters' => 250,
-							'content_characters' => 0,
-							'title_content_divider' => '<br />',
-							'title_before'	=>	'',
-							'title_after'	=>	'',
-							'global_before'	=>	'<ul class="sidebar-list-tabs left relative">',
-							'global_after'	=>	'</ul>',
-							'before'	=>	'<li>',
-							'after'	=>	'</li>',
-							'title_link' => 'yes',
-							'show_avatars' => 'no',
-							'avatar_size' => 16,
-							'posttype' => 'post'
-						);
+		$defaults = array(
+			'number' => 5,
+			'title_characters' => 250,
+			'content_characters' => 0,
+			'title_content_divider' => '<br />',
+			'title_before'	=> '',
+			'title_after'	=> '',
+			'global_before'	=> '<ul class="sidebar-list-tabs left relative">',
+			'global_after'	=> '</ul>',
+			'before' => '<li>',
+			'after'	=> '</li>',
+			'title_link' => 'yes',
+			'show_avatars' => 'no',
+			'avatar_size' => 16,
+			'posttype' => 'post',
+		);
 
-		extract(shortcode_atts($defaults, $atts));
+		extract( shortcode_atts( $defaults, $atts ) );
 
 		$html = '';
 
-		$html .= $this->solr_display_recent_posts( $number, $title_characters, $content_characters, $title_content_divider, $title_before, $title_after, $global_before, $global_after, $before, $after, $title_link, $show_avatars, $avatar_size, $posttype, false);
+		$html .= $this->solr_display_recent_posts( $number, $title_characters, $content_characters, $title_content_divider, $title_before, $title_after, $global_before, $global_after, $before, $after, $title_link, $show_avatars, $avatar_size, $posttype, false );
 
 		return $html;
 
 	}
-
 }
 
-function solr_display_recent_posts($tmp_number,$tmp_title_characters = 0,$tmp_content_characters = 0,$tmp_title_content_divider = '<br />',$tmp_title_before,$tmp_title_after,$tmp_global_before,$tmp_global_after,$tmp_before,$tmp_after,$tmp_title_link = 'no',$tmp_show_avatars = 'yes', $tmp_avatar_size = 16, $posttype = 'post', $output = true) {
+function solr_display_recent_posts( $tmp_number, $tmp_title_characters = 0, $tmp_content_characters = 0, $tmp_title_content_divider = '<br />', $tmp_title_before, $tmp_title_after, $tmp_global_before, $tmp_global_after, $tmp_before, $tmp_after, $tmp_title_link = 'no', $tmp_show_avatars = 'yes', $tmp_avatar_size = 16, $posttype = 'post', $output = true ) {
 	global $solractivityfeedshortcode;
 
 	$solractivityfeedshortcode->solr_display_recent_posts( $tmp_number, $tmp_title_characters, $tmp_content_characters, $tmp_title_content_divider, $tmp_title_before, $tmp_title_after, $tmp_global_before, $tmp_global_after, $tmp_before, $tmp_after, $tmp_title_link, $tmp_show_avatars, $tmp_avatar_size, $posttype, $output );
 }
 
-$solractivityfeedshortcode = new solractivityfeedshortcode();
+$solractivityfeedshortcode = new SolrActivityFeedShortcode();
